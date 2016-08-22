3ea2ea9b9f1969a1f7871bbbcd6e31ecb4b24896
Justin Malunay <jrcmalunay@gmail.com>
2016-08-22 11:27:11 -0700

    inial commit

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://jrmalunay.github.io'
+SITEURL = 'http://jrcmalunay.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
